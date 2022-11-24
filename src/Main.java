f21f79d1c285987d7576973e9f0c2ccaa36725c7
seoljiwon <suljiwon@naver.com>
2022-11-24 17:51:42 +0900

    feat: add source data, change path to relative

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) throws Exception {
-		String path = "C:\\Users\\Àlex Esteban\\Desktop\\Uni\\INSA-IST-SOE\\Sensors.csv";
+		String path = "./src/data/Sensors.csv";
 		Scanner sensors = new Scanner(new File(path));
 		System.out.println(sensors.next());
 		while (sensors.hasNext()) {
