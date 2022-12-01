5b98aa18ccf1d5ad5840556e5ff504a096035c0c
alexestebanupc <alex.esteban.rodriguez@estudiantat.upc.edu>
2022-12-01 10:03:07 +0100

    feat: calculate data

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,48 @@
-import java.io.*;  
+import java.io.*;
 import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) throws Exception {
-		String path = "./src/data/Sensors.csv";
-		Scanner sensors = new Scanner(new File(path));
-		System.out.println(sensors.next());
-		while (sensors.hasNext()) {
-			String[] line = sensors.next().split(";");
-			System.out.println(line[1]);  
-		}   
-		sensors.close();    
-	}
+    public static void main(String[] args) throws Exception {
+
+        // ********** open files **********
+        String sensorsPath = "./src/data/Sensors.csv";
+        Scanner sensors = new Scanner(new File(sensorsPath));
+
+        String attributesPath = "./src/data/AttributeType.csv";
+        Scanner attributes = new Scanner(new File(attributesPath));
+
+        String dataPath = "./src/data/data_new.csv";
+        Scanner data = new Scanner(new File(dataPath));
+
+        // ********** main **********
+
+        // mean value in a specific location of a specific attribute
+
+        String location = "Sensor0";
+        String attr = "O3";
+
+        int counter = 0;
+        int sum = 0;
+        System.out.println(data.next());
+        while (data.hasNext()) {
+            String[] columns = data.next().split(";");
+            // System.out.println(columns[1]);
+            // System.out.println(columns[2]);
+            if (columns[1] == location && columns[2] == attr) {
+                sum += Double.parseDouble(columns[3]);
+                counter++;
+            }
+        }
+        System.out.println(counter);
+        if (counter != 0)
+            System.out.println(sum/counter);
+
+
+        // ********** close files **********
+        sensors.close();
+        attributes.close();
+        data.close();
+
+    }
 }
