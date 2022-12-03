b1041096d43d2df897ba636ec7c7e0894a59c522
alexestebanupc <alex.esteban.rodriguez@estudiantat.upc.edu>
2022-12-03 16:55:13 +0100

    feat: mean value updated

--- src/Main.java
+++ src/Main.java
@@ -12,7 +12,7 @@ public class Main {
         String attributesPath = "./src/data/AttributeType.csv";
         Scanner attributes = new Scanner(new File(attributesPath));
 
-        String dataPath = "./src/data/data_new.csv";
+        String dataPath = "./src/data/data.csv";
         Scanner data = new Scanner(new File(dataPath));
 
         // ********** main **********
@@ -24,19 +24,29 @@ public class Main {
 
         int counter = 0;
         int sum = 0;
-        System.out.println(data.next());
         while (data.hasNext()) {
             String[] columns = data.next().split(";");
-            // System.out.println(columns[1]);
-            // System.out.println(columns[2]);
-            if (columns[1] == location && columns[2] == attr) {
+            if (columns[1].equals(location) && columns[2].equals(attr)) {
                 sum += Double.parseDouble(columns[3]);
                 counter++;
             }
         }
-        System.out.println(counter);
-        if (counter != 0)
-            System.out.println(sum/counter);
+
+        if (counter != 0) {
+            String unit = "";
+            String description = "";
+            boolean found = false;
+            while (attributes.hasNext() && !found) {
+                String[] columns = attributes.next().split(";");
+                if (columns[0].equals(attr)) {
+                    found = true;
+                    unit = columns[1];
+                    description = columns[2];
+                }
+            }
+            System.out.println("----------\nMean " + description + " (" + attr + ") in " + location + ": " + sum / counter + " " + unit + "\n----------");
+        }
+        else System.out.println("Not found.");
 
 
         // ********** close files **********
