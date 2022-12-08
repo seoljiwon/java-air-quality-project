931056464b0807ae4e71adcc024550ee5d996da9
alexestebanupc <alex.esteban.rodriguez@estudiantat.upc.edu>
2022-12-08 08:34:32 +0100

    feat: get date function

--- src/Calculator.java
+++ src/Calculator.java
@@ -56,4 +56,55 @@ public class Calculator {
 
         return sum / counter;
     }
+
+    public double getAverageBySensorDay(String sensorId, String attributeId, String date) throws FileNotFoundException {
+        // calculates the mean value of a given attribute (chemical formula) and a given location (sensor)
+        // on a specific day
+
+        // open files
+        String dataPath = "./src/data/data.csv";
+        Scanner data = new Scanner(new File(dataPath));
+
+        // calculate value
+
+        // First timestamp: 2017-01-01T00:01:20.6090000
+        // Last timestamp:  2017-12-30T23:30:10.9830000
+
+        int counter = 0;
+        double sum = 0;
+        while (data.hasNext()) {
+            String[] columns = data.next().split(";");
+
+            if (columns[1].equals(sensorId) && columns[2].equals(attributeId) && date.equals(columns[0].split("T")[0])) {
+                sum += Double.parseDouble(columns[3]);
+                counter++;
+            }
+        }
+
+        if (counter == 0) {
+            throw new ArithmeticException("divide by zero");
+        }
+
+        // close files
+        data.close();
+
+        return sum / counter;
+    }
+
+    String getDate(int month, int day) {
+        // returns the date in a correct format
+
+        String date;
+        if (month < 10) {
+            if (day < 10)
+                date = "2017-0" + Integer.toString(month) + "-0" + Integer.toString(day);
+            else date = "2017-0" + Integer.toString(month) + "-" + Integer.toString(day);
+        }
+        else {
+            if (day < 10)
+                date = "2017-" + Integer.toString(month) + "-0" + Integer.toString(day);
+            else date = "2017-" + Integer.toString(month) + "-" + Integer.toString(day);
+        }
+        return date;
+    }
 }
